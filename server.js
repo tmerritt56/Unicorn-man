618a1118873dc26d099811889519cb15f54ac0fc
Apon wuz here <frost_bird@hotmail.com>
2022-09-19 21:07:48 -0500

    made a heroku

--- server.js
+++ server.js
@@ -5,27 +5,27 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
-const sequelize = requie('./config/connection');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
-    secret: 'Super secret secret',
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db:sequelize
-    })
+  secret: 'Super secret secret',
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 const app = express();
 const PORT = process.env.PORT || 5001;
 const hbs = exphbs.create({ helpers });
 app.use(session(sess));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(routes);
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars'); 
+app.set('view engine', 'handlebars');
 
-sequelize.sync({ force:false }).then(()=> {
-    app.listen(PORT, () => console.log('Now Unicorns are listening on 5001'));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now Unicorns are listening on 5001'));
 });
