b37c5d442e56664f458686f7b8d5d248ca4c0359
Apon wuz here <frost_bird@hotmail.com>
2022-09-21 18:58:12 -0500

    wednesday night bawling

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -1,140 +1,43 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require('../../models');
+const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
-  User.findAll({
-    attributes: { exclude: ['password'] },
-  })
-    .then((dbUserData) => res.json(dbUserData))
+  Comment.findAll({})
+    .then((dbCommentData) => res.json(dbCommentData))
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-router.get('/:id', (req, res) => {
-  User.findOne({
-    attributes: { exclude: ['password'] },
-    where: {
-      id: req.params.id,
-    },
-    include: [
-      {
-        model: Post,
-        attributes: ['id', 'title', 'post_content', 'creation_date'],
-      },
-      {
-        model: Comment,
-        attributes: ['id', 'comment_content', 'creation_date'],
-        include: {
-          model: Post,
-          attributes: ['username'],
-        },
-      },
-    ],
-  })
-    .then((dbUserData) => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
-      }
-      res.json(dbUserData);
+router.post('/', withAuth, async (req, res) => {
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id,
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-//needs to be adjusted as we fill out more content
-router.post('/', (req, res) => {
-  User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-    github: req.body.github,
-  }).then((dbUserData) => {
-    req.session.save(() => {
-      req.session.user_id = dbUserData.id;
-      req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
-
-      res.json(dbUserData);
-    });
-  });
-});
-
-router.post('/login', (req, res) => {
-  User.findOne({
-    where: {
-      email: req.body.email,
-    },
-  }).then((dbUserData) => {
-    if (!dbUserData) {
-      res.status(400).json({ message: 'Incorrect "Email" or Password!' });
-      return;
-    }
-
-    const vaildPassword = dbUserData.checkPassword(req.body.password);
-
-    if (!vaildPassword) {
-      res.status(400).json({ message: 'Incorrect Email or "Password"!' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = dbUserData.id;
-      req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
-
-      res.json({ user: dbUserData, message: 'Login Successful!' });
-    });
-  });
-});
-
-router.post('/logout', (req, res) => {
-  if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
+      .then((dbCommentData) => res.json(dbCommentData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
   }
 });
 
-router.put('/:id', withAuth, (req, res) => {
-  User.update(req.body, {
-    individualHooks: true,
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((dbUserData) => {
-      if (!dbUserData[0]) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
 router.delete('/:id', withAuth, (req, res) => {
-  User.destroy({
+  Comment.destroy({
     where: {
       id: req.params.id,
     },
   })
-    .then((dbUserData) => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: 'No comment found with this id' });
         return;
       }
-      res.json(dbUserData);
+      res.json(dbCommentData);
     })
     .catch((err) => {
       console.log(err);
