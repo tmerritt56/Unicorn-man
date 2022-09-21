1d0d24a847d04d4ad1d4802749455ccd6612133d
Tara Merritt <104723943+tmerritt56@users.noreply.github.com>
2022-09-21 17:36:02 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,6 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
   secret: 'Super secret secret',
-  cookie: {
-    maxAge: 300000,
-    httpOnly:true,
-    secure:false,
-    sameSite:'strict',
-  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
