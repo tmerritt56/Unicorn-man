2f562693a76f38d3b4a7b45b534661566c942b9a
Apon wuz here <frost_bird@hotmail.com>
2022-09-22 21:40:23 -0500

    he did it

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -12,18 +12,47 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', withAuth, async (req, res) => {
-  if (req.session) {
-    Comment.create({
-      comment_content: req.body.comment_content,
+  try {
+    const dbCommentData = await Comment.create({
+      ...req.body.comment_content,
       post_id: req.body.post_id,
       user_id: req.session.user_id,
-    })
-      .then((dbCommentData) => res.json(dbCommentData))
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json(err);
-      });
+    });
+    res.status(200).json(dbCommentData);
+  } catch (err) {
+    res.status(400).json(err);
   }
+  // if (req.session) {
+  //   Comment.create({
+  //     comment_content: req.body.comment_content,
+  //     post_id: req.body.post_id,
+  //     user_id: req.session.user_id,
+  //   })
+  //     .then((dbCommentData) => res.json(dbCommentData))
+  //     .catch((err) => {
+  //       console.log(err);
+  //       res.status(400).json(err);
+  //     });
+  // }
+});
+
+router.get('/:id', withAuth, (req, res) => {
+  Comment.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: 'comment not found' });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', withAuth, (req, res) => {
